406f048d1193725fdff30112f81483702fc16d0a
Adi <adi.weiss08@gmail.com>
2026-02-27 17:45:55 +0200

    add

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import HomePage from "@/react-app/pages/Home";
+import { useEffect } from "react";
 
 export default function App() {
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Assistance"],
+      },
       colors: {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
